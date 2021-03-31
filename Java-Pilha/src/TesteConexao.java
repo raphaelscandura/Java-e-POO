8a7871bfa961fc2b35d17abe194a3246fd445503
Raphael <raphaelscandura@hotmail.com>
2021-03-31 16:13:21 -0300

    Finally e Closeable

---
+++ Java-Pilha/src/TesteConexao.java
@@ -0,0 +1,29 @@
+
+public class TesteConexao {
+
+	public static void main(String[] args) {
+		
+		try(Conexao con = new Conexao()){
+			con.leDados();
+		}catch (IllegalStateException ex) {
+			System.out.println("Deu erro na conexão...");
+		}
+	
+		
+		
+//_________________________________________________________
+
+//		Conexao con = null;
+//
+//		try {
+//			con = new Conexao();
+//			con.leDados();
+//
+//		} catch (IllegalStateException ex) {
+//			System.out.println("Deu erro na conexão...");
+//		} finally {
+//			con.fecha();
+//		}
+//
+	}
+}
