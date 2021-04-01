82f634b0cedbff0e3659cc486819edeadd2a38c0
Raphael <raphaelscandura@hotmail.com>
2021-04-01 13:04:08 -0300

    Classe Object e java.lang

---
+++ Java-Pilha/src/TesteString.java
@@ -0,0 +1,22 @@
+
+public class TesteString {
+
+	public static void main(String[] args) {
+
+
+		String nome = "Raphael";
+		
+		String alterada = nome.toLowerCase();
+		
+		System.out.println(alterada);
+		System.out.println(nome);
+		
+		String alterada2 = nome.replace("R", "Z");
+		
+		System.out.println(alterada2);
+		System.out.println(nome);
+				
+
+	}
+
+}
