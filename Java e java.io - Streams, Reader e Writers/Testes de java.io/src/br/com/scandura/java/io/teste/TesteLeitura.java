96b50a9c2b1e86391f7759241a54ef22febb49bc
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-16 16:00:32 -0300

    Teste básico de leitura de um arquivo externo

--- Java e java.io - Streams, Reader e Writers/Testes de java.io/src/br/com/scandura/java/io/teste/TesteLeitura.java
+++ Java e java.io - Streams, Reader e Writers/Testes de java.io/src/br/com/scandura/java/io/teste/TesteLeitura.java
@@ -4,17 +4,26 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.InputStream;
 
 public class TesteLeitura {
     public static void main(String[] args) throws IOException {
         
-        FileInputStream fis = new FileInputStream("lorem.txt");
-        InputStreamReader isr = new InputStreamReader(fis);
+        InputStream fis = new FileInputStream("lorem.txt");
+        Reader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
         String linha = br.readLine();
+
+        System.out.println();      
+        
+        while(linha != null){
+            System.out.println(linha);
+            linha = br.readLine();
+        }
+
         System.out.println();
-        System.out.println(linha);
-        System.out.println();
+
         br.close();
     }
 }
