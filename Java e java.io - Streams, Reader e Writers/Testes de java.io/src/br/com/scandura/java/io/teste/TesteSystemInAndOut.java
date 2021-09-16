fc47bb91249db427e41f5c08528b70771522c3c6
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-16 17:09:10 -0300

    Testando i/o no console

---
+++ Java e java.io - Streams, Reader e Writers/Testes de java.io/src/br/com/scandura/java/io/teste/TesteSystemInAndOut.java
@@ -0,0 +1,36 @@
+package br.com.scandura.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.InputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.io.OutputStream;
+
+public class TesteSystemInAndOut {
+    public static void main(String[] args) throws IOException {
+
+        OutputStream fos = System.out;
+        Writer osw = new OutputStreamWriter(fos);
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        InputStream fis = System.in;
+        Reader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+
+        String linha = br.readLine();
+
+        while (linha != null && !linha.isEmpty()) {
+            bw.write(linha);
+            bw.newLine();
+            linha = br.readLine();
+        }
+
+        bw.close();
+        br.close();
+
+    }
+}
