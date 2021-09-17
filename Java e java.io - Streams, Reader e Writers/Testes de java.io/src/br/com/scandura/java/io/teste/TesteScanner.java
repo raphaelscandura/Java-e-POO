36570dfcda637bcd4db4cbabf7b9bbdcdce6aa0a
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-17 12:17:05 -0300

    Testando Delimiter e tipos diferentes de next()

--- Java e java.io - Streams, Reader e Writers/Testes de java.io/src/br/com/scandura/java/io/teste/TesteScanner.java
+++ Java e java.io - Streams, Reader e Writers/Testes de java.io/src/br/com/scandura/java/io/teste/TesteScanner.java
@@ -1,6 +1,7 @@
 package br.com.scandura.java.io.teste;
 
 import java.io.FileNotFoundException;
+//import java.util.Locale;
 import java.util.Scanner;
 import java.io.File;
 
@@ -11,6 +12,24 @@ public class TesteScanner{
         while(scanner.hasNextLine()){
             String linha = scanner.nextLine();
             System.out.println(linha);
+
+            //String[] dados = linha.split(",");
+            //System.out.println(dados[3]);
+
+            Scanner linhaScanner = new Scanner(linha);
+            //linhaScanner.useLocale(Locale.US); //Caso a máquina não esteja com a região configurada como Estados Unidos, usar isso para resolver a InputMismatchException do Double
+            linhaScanner.useDelimiter(",");
+
+            String tipoDeConta = linhaScanner.next();
+            int numeroAgencia = linhaScanner.nextInt();
+            int numeroConta = linhaScanner.nextInt();
+            String nomeTitular = linhaScanner.next();
+            double valorSaldo = linhaScanner.nextDouble();
+
+            System.out.println("Tipo da conta: " + tipoDeConta + " Número da agência: " + numeroAgencia + " Número da conta: " + numeroConta + " Nome do titular: " + nomeTitular + " Saldo da conta: " + valorSaldo);
+
+            linhaScanner.close();
+
         }
         
         scanner.close();
