824ff3796056019cb7d013266bd3e98d7efd891e
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-17 17:06:34 -0300

    Manipulando listas encapsuladas

--- Java Collections - Dominando Listas, Sets e Mapas/gerenciador-de-cursos/src/br/com/scandura/TestandoCurso.java
+++ Java Collections - Dominando Listas, Sets e Mapas/gerenciador-de-cursos/src/br/com/scandura/TestandoCurso.java
@@ -1,11 +1,25 @@
 package br.com.scandura;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TestandoCurso {
     public static void main(String[] args) {
         Curso javaColecoes = new Curso("Dominando as coleções do java", "Raphael Scandura");
-        javaColecoes.adiciona(new Aula("Introdução ao java", 8));
+        javaColecoes.adiciona(new Aula("Introdução ao java", 38));
+        javaColecoes.adiciona(new Aula("Entendendo conceitos básicos", 18));
+        javaColecoes.adiciona(new Aula("Se aprofundando no conteúdo", 28));
 
-        System.out.println(javaColecoes.getAulas());
+        List<Aula> aulasImutaveis = javaColecoes.getAulas();
+        System.out.println(aulasImutaveis);
+
+        List<Aula> aulasMutaveis = new ArrayList<>(aulasImutaveis);
+        Collections.sort(aulasMutaveis);
+        System.out.println(aulasMutaveis);
+
+        System.out.println("Tempo total do curso: " + javaColecoes.getTempoTotal() + " minutos");
+        
     }
     
 }
