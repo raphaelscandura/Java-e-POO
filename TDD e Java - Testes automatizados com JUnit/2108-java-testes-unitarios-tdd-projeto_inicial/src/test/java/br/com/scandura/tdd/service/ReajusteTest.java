17aa350519c216bf329113fcedbc9f37cdf90606
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-19 15:17:17 -0300

    refactoring

--- TDD e Java - Testes automatizados com JUnit/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/scandura/tdd/service/ReajusteTest.java
+++ TDD e Java - Testes automatizados com JUnit/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/scandura/tdd/service/ReajusteTest.java
@@ -5,44 +5,41 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.scandura.tdd.modelo.Desempenho;
 import br.com.scandura.tdd.modelo.Funcionario;
 
 class ReajusteTest {
+	
+	public BigDecimal salario;
+	public Funcionario funcionario;
+	public ReajusteService service;
+	
+	@BeforeEach
+	public void inicializar() {
+		this.salario = new BigDecimal("1000.00");
+		this.funcionario = new Funcionario("", LocalDate.now(), this.salario);
+		this.service = new ReajusteService();
+	}
 
 	@Test
-	void funcionarioRecebeReajusteDe3PorcentoPoisDesempenhoDeixouADesejar() {
-		BigDecimal salarioMock = new BigDecimal("1000.00");
-		Funcionario mock = new Funcionario("", LocalDate.now(), salarioMock);
-		ReajusteService service = new ReajusteService();
-		
-		service.calcularReajuste(mock, Desempenho.A_DESEJAR);		
-		
-		assertEquals(new BigDecimal("1030.00"), mock.getSalario());
+	void funcionarioRecebeReajusteDe3PorcentoPoisDesempenhoDeixouADesejar() {		
+		service.calcularReajuste(funcionario, Desempenho.A_DESEJAR);		
+		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 	}
 	
 	@Test
 	void funcionarioRecebeReajusteDe15PorcentoPoisDesempenhoFoiBom() {
-		BigDecimal salarioMock = new BigDecimal("1000.00");
-		Funcionario mock = new Funcionario("", LocalDate.now(), salarioMock);
-		ReajusteService service = new ReajusteService();
-		
-		service.calcularReajuste(mock, Desempenho.BOM);		
-		
-		assertEquals(new BigDecimal("1150.00"), mock.getSalario());
+		service.calcularReajuste(funcionario, Desempenho.BOM);		
+		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
 	}
 	
 	@Test
 	void funcionarioRecebeReajusteDe20PorcentoPoisDesempenhoFoiOtimo() {
-		BigDecimal salarioMock = new BigDecimal("1000.00");
-		Funcionario mock = new Funcionario("", LocalDate.now(), salarioMock);
-		ReajusteService service = new ReajusteService();
-		
-		service.calcularReajuste(mock, Desempenho.OTIMO);		
-		
-		assertEquals(new BigDecimal("1200.00"), mock.getSalario());
+		service.calcularReajuste(funcionario, Desempenho.OTIMO);		
+		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
 	}
 
 }
