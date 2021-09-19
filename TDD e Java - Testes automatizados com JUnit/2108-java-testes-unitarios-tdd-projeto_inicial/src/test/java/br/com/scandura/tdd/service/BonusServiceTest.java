2111c49bc6e41713eabfba5aab24d5fc45ff0ac2
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-19 15:05:43 -0300

    Tratamento de exceptions com testes JUnit

--- TDD e Java - Testes automatizados com JUnit/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/scandura/tdd/service/BonusServiceTest.java
+++ TDD e Java - Testes automatizados com JUnit/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/scandura/tdd/service/BonusServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.scandura.tdd.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -14,10 +15,7 @@ class BonusServiceTest {
 	@Test
 	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("", LocalDate.now(), new BigDecimal("20000")));
-		
-		assertEquals(new BigDecimal("0.00"), bonus);
-		
+		assertThrows(IllegalArgumentException.class, () -> service.calcularBonus(new Funcionario("", LocalDate.now(), new BigDecimal("20000"))));		
 	}
 	
 	@Test
