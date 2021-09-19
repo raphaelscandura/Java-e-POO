08beb360a085f097c574f08eb45218cf4d47241d
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-19 11:19:59 -0300

    Preparando o ambiente

---
+++ TDD e Java - Testes automatizados com JUnit/Testando TDD/src/br/com/scandura/Calculadora.java
@@ -0,0 +1,21 @@
+package br.com.scandura;
+
+public class Calculadora {
+
+    public double somar(double a, double b){
+        return a + b;
+    }
+
+    public double subtrair(double a, double b){
+        return a - b;
+    }
+
+    public double multiplicar(double a, double b){
+        return a * b;
+    }
+
+    public double dividr(double a, double b){
+        return a / b;
+    }
+    
+}
